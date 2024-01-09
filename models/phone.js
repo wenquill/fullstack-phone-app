a87f25e6cdcbd29fbbe2e9d880e898cdebf110b7
victoria <vikabirukova22@gmail.com>
2024-01-09 12:23:14 +0200

    edit model

--- models/phone.js
+++ models/phone.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model, Op } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Phone extends Model {
     /**
@@ -9,21 +7,47 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       // define association here
     }
   }
-  Phone.init({
-    brand: DataTypes.STRING,
-    model: DataTypes.STRING,
-    manufacturedYear: DataTypes.DATEONLY,
-    ramSize: DataTypes.NUMBER,
-    processor: DataTypes.STRING,
-    screenDiagonal: DataTypes.NUMBER,
-    hasNFC: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Phone',
-  });
+  Phone.init(
+    {
+      brand: {
+        type: DataTypes.STRING(32),
+        allowNull: false,
+      },
+      model: {
+        type: DataTypes.STRING(32),
+        allowNull: false,
+      },
+      manufacturedYear: {
+        type: DataTypes.DATEONLY,
+        validate: {
+          [Op.between]: [1970, new Date().getFullYear()],
+        },
+      },
+      ramSize: {
+        type: DataTypes.INTEGER,
+        validate: { [Op.gt]: 0 },
+      },
+      processor: {
+        type: DataTypes.STRING,
+        validate: { [Op.gt]: 0 },
+      },
+      screenDiagonal: {
+        type: DataTypes.REAL,
+        validate: { [Op.gt]: 0 },
+      },
+      hasNFC: {
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Phone',
+      underscored: true,
+    }
+  );
   return Phone;
 };
