f961ae4c9d16d19247c8f912d713d536296dfefe
victoria <vikabirukova22@gmail.com>
2024-01-09 13:53:34 +0200

    edit controllers

--- controllers/phoneControllers.js
+++ controllers/phoneControllers.js
@@ -1,9 +1,99 @@
-module.exports.createPhone = (req, res, next) => {};
+const { Phone } = require('./../models');
 
-module.exports.getAllPhones = (req, res, next) => {};
+module.exports.createPhone = async (req, res, next) => {
+  const { body } = req;
 
-module.exports.getPhone = (req, res, next) => {};
+  try {
+    const createdPhone = await Phone.create(body);
 
-module.exports.updatePhone = (req, res, next) => {};
+    if (!createdPhone) {
+      return res.status(400).send('Something went wrong...');
+    }
 
-module.exports.deletePhone = (req, res, next) => {};
+    res.status(201).send(createdPhone);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getAllPhones = async (req, res, next) => {
+  const queryParams = req.query;
+  const page = queryParams.page || 1;
+  const pageSize = queryParams.pageSize || 10;
+
+  const whereConditions = {};
+  for (const key in queryParams) {
+    if (
+      queryParams.hasOwnProperty(key) &&
+      key !== 'page' &&
+      key !== 'pageSize'
+    ) {
+      whereConditions[key] = {
+        [Op.eq]: queryParams[key],
+      };
+    }
+  }
+
+  const offset = (page - 1) * pageSize;
+
+  try {
+    const foundPhones = await Phone.findAll({
+      where: whereConditions,
+      limit: parseInt(pageSize),
+      offset: parseInt(offset),
+    });
+
+    if (!foundPhones) {
+      return res.status(404).send('Phones not found ):');
+    }
+
+    res.status(200).send(foundPhones);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getPhone = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const foundPhone = await Phone.findByPk(id);
+
+    if (!foundPhone) {
+      return res.status(404).send('Phone not found ):');
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updatePhone = async (req, res, next) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  try {
+    const updatedPhone = await Phone.update(body, {
+      where: {
+        id: id,
+      },
+    });
+
+    res.status(200).send(updatedPhone);
+  } catch (err) {}
+};
+
+module.exports.deletePhone = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const deletedPhone = await Phone.destroy(id);
+
+    if (!deletedPhone) {
+      return res.status(404).send('PHone not found ):');
+    }
+
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+};
