0734e08a0108c70d26467ec15fde211195e02a88
victoria <vikabirukova22@gmail.com>
2024-01-09 13:14:39 +0200

    init routers

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = require('./app');
 
 const httpServer = http.createServer(app);
 
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 5000;
 
-httpServer.listen((PORT, HOST) => {
+httpServer.listen(PORT, HOST, () => {
   console.log(`Server is listening host ${HOST} at port ${PORT}`);
 });
