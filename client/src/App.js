c96551cb67d3d76950ea7f8621b765fe4803c9bf
victoria <vikabirukova22@gmail.com>
2024-01-22 14:20:56 +0200

    Add prettier configuration

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
+import './App.css'
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      app
-    </div>
-  );
+function App () {
+  return <div className='App'>app</div>
 }
 
-export default App;
+export default App
