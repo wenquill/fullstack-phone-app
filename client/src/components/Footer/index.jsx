cf5887136e01531f09b82f3a99e631956da76e2a
victoria <vikabirukova22@gmail.com>
2024-01-23 19:37:00 +0200

    Add footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import {
+  FaFacebookSquare,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter
+} from 'react-icons/fa'
+import styles from './Footer.module.scss';
 
-function Footer() {
+function Footer () {
   return (
-    <footer>Footer</footer>
-  )
+    <footer className={styles.container}>
+      <div>&copy;2024 Phones App. All rights reserved</div>
+      <div className={styles.iconsContainer}>
+        <FaFacebookSquare fill='#fff' className={styles.icon} />
+        <FaInstagram fill='#fff' className={styles.icon} />
+        <FaYoutube fill='#fff' className={styles.icon} />
+        <FaTwitter fill='#fff' className={styles.icon} />
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
